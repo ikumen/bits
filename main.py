f8bbd0bafc7e6417955b854a84e7c50a268ce895
ikumen <ikumen@gnoht.com>
2016-10-20 23:20:15 -0700

    More clean up. Add tags page

--- main.py
+++ main.py
@@ -18,17 +18,33 @@ gist_post_converter = tasks.GistToPostConverter(
 
 @app.route('/admin/update')
 def admin_update():
+	# TODO: move user_id to config
 	deferred.defer(gist_post_converter.converts, user_id='ikumen')
 	return redirect(url_for('index'))
 
+
 @app.route('/')
 def index():
-	# TODO: move user_id to config
-	#deferred.defer(gist_handler.all, user_id='ikumen')
 	posts = models.Post.query().order(-models.Post.created_at)
 	return render_template('index.html', posts=posts, now=date.today())
 
 
+@app.route('/tags')
+def tags():
+	posts = (models.Post.query()
+		.order(models.Post.tags)
+		.fetch(projection=['title', 'slug', 'tags']))
+	# TODO: OrderedDict?
+	tags_posts = {}
+	tags = []
+	for post in posts:
+		for tag in post.tags:
+			if tag not in tags_posts:
+				tags_posts[tag] = []
+				tags.append(tag)
+			tags_posts[tag].append(post)
+	# TODO: DRY now creation/cache for jinja
+	return render_template('tags.html', tags=tags, tags_posts=tags_posts, now=date.today())
 
 
 @app.route('/posts/<path:slug>')
@@ -36,68 +52,9 @@ def posts(slug):
 	post = models.Post.query(models.Post.slug == slug).get()
 	return render_template('posts.html', post=post, now=date.today())
 
+
 @app.route('/about')
 def about():
 	return render_template('about.html', now=date.today())
 
-'''
 
-@app.route('/fetch')
-def fetch():
-	deferred.defer(fetch_gists)
-	return redirect(url_for('index'))
-
-@app.errorhandler(500)
-def server_error(e):
-	# log error and stacktrace
-	logging.exception('Oops!')
-	return 'Oops', 500
-
-
-
-def update_gist(gist_id):
-	result = urlfetch.fetch('https://api.github.com/users/ikumen/gists/' + gist_id,
-		validate_certificate=True,
-		headers={'Authorization': 'token '})
-	if result.status_code == 200:
-		print('--------------> ')
-		print(result.content)
-
-
-def fetch_gists(token):
-	try:
-		result = urlfetch.fetch('https://api.github.com/users/ikumen/gists', 
-			validate_certificate=True,
-			headers={'Authorization': 'token '})
-		print('------- status')
-		print(result.status_code)
-		if result.status_code == 200:
-			gists = json.loads(result.content)
-			for gist in gists:
-				if '.bits' in gist['files']:
-					# .bits file contains meta data about this gists/post
-					bits = yaml.load(gist['files']['.bits'])
-					files = {}
-					for file in gist['files']:
-						files[gist['files'][file]['filename']] = {
-							'name': gist['files'][file]['filename'],
-							'url': gist['files'][file]['raw_url'],
-							'size': gist['files'][file]['size']
-						}
-
-					gistModel = Gist(id=gist['id'],
-						description=gist['description'],
-						created_at=dateutil.parser.parse(gist['created_at'], ignoretz=True),
-						updated_at=dateutil.parser.parse(gist['updated_at'], ignoretz=True),
-						files=files)
-
-					deferred.defer(update_gist, gist['id'])
-
-					print('saving gist....')
-					gistModel.put()
-		else:
-			print(result.status_code)
-	except urlfetch.Error:
-		logging.exception('Caught exception while fetching: https://api.github.com/users/ikumen/gists')
-
-'''
