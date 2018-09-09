fd5c9bb46bb3a08e1e70f6f65056f8f82d027756
ikumen <ikumen@gnoht.com>
2018-09-08 23:07:27 -0700

    Moved security to separate mod.

---
+++ server/security.py
@@ -0,0 +1,78 @@
+import logging
+
+from flask import session, jsonify, redirect
+from functools import wraps
+from services import github, user_service
+from helpers import handle_error
+
+
+log = logging.getLogger(__name__)
+
+USER_SESS_ATTR = 'user'
+OAUTH_ATTR_NAME = 'oauth'
+
+@github.access_token_getter
+def token_getter():
+    oauth_token = session.get(OAUTH_ATTR_NAME)
+    if oauth_token is None:
+        oauth_token = session.get(USER_SESS_ATTR, {}).get(OAUTH_ATTR_NAME)
+    print(oauth_token)
+    return oauth_token
+
+
+def authorized(f):
+    """Decorator for methods that require an authorized user. Decorated
+    route handler will get user passed in.
+    """
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        user = current_user()
+        if not user:
+            return redirect('/signout')
+        else:
+            return f(user, **kwargs)
+    return decorated
+
+
+def authorization_start(f):
+    """Decorator for method that starts authorization."""
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if current_user() is None:
+            log.debug('User is not authenticated, starting authentication!')
+            return github.authorize(scope='read:user,gist')
+        return f(*args, **kwargs)
+    return decorated
+
+
+def post_authorization(f):
+    """Decorator for method that handles authorization."""
+    @wraps(f)
+    def decorated(oauth_token, **kwargs):
+        if oauth_token is None:
+            return handle_error(message='Authorization failed!', status=401)
+
+        # temporarily save access token
+        session[OAUTH_ATTR_NAME] = oauth_token
+
+        # let's get user login info
+        user_info = github.get('/user')
+        if 'login' not in user_info:
+            return handle_error(message='Unable to load user!', status=401)
+
+        # remove temporary token
+        session.pop(OAUTH_ATTR_NAME)
+        # persists/update user associated with this token
+        user = {'_id': user_info['login'], 'oauth': oauth_token}
+        user_service.save(user)
+
+        session[USER_SESS_ATTR] = user
+        return f(user, **kwargs)
+
+    return decorated
+
+
+def current_user():
+    return session.get(USER_SESS_ATTR)
+
+
