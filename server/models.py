d97888f016b0983e86a2db8153ada0ccac92306f
ikumen <ikumen@gnoht.com>
2018-09-25 23:08:35 -0700

    Finishing porting most of model and service functionality to GAE's ndb.

--- server/models.py
+++ server/models.py
@@ -8,12 +8,9 @@ from .helpers import JSONSerializable
 
 class GAEModel(JSONSerializable, ndb.Model):
     _default_indexed = False
-    __model__ = None
 
     def __init__(self, **kwargs):
         super(GAEModel, self).__init__(**kwargs)
-        if self.__model__ is None:
-            raise TypeError('Model (__model__) is not defined!')
 
     @classmethod
     def id_to_key(cls, id):
@@ -34,19 +31,6 @@ class GAEModel(JSONSerializable, ndb.Model):
         return ndb.Key(urlsafe=urlsafe_key)
 
     @classmethod
-    def _isinstance(cls, model, raise_error=True):
-        """Check if given model is of same type as implementing class.
-
-        @param model the model instance to check
-        @param raise_error indicate if we should raise an error on type mismatch
-        @returns True if model is instance of given class
-        """
-        result = isinstance(model, cls)
-        if not result and raise_error:
-            raise ValueError('%s is not of type %s' % (model, cls))
-        return result
-
-    @classmethod
     def create(cls, **kwargs):
         """Creates a new instance of implementing class and saves it.
 
@@ -58,30 +42,17 @@ class GAEModel(JSONSerializable, ndb.Model):
         return instance
 
     @classmethod
-    def list(cls, parent_key=None, limit=50):
+    def list(cls, limit=50):
         """Returns all instances of implementing class.
 
         @param parent_key optional ancestor key to filter by
         @param limit optional fetch limit, defaults to 50
         """
-        query = cls.query() if parent_key is None else cls.query(ancestor=ndb.Key(urlsafe=parent_key))
+        query = cls.query()
         return query.fetch(limit)
 
     @classmethod
-    def get_by_key(cls, key):
-        """Returns instance of implementing class identified by given urlsafe key.
-        Key should be in urlsafe format, see:
-        https://cloud.google.com/appengine/docs/python/ndb/creating-entities#Python_retrieving_entities
-
-        @param key identifying key
-        """
-        try:
-            return ndb.Key(urlsafe=key).get()
-        except ProtocolBufferDecodeError:
-            return None
-
-    @classmethod
-    def get_by_id(cls, id):
+    def get(cls, id):
         """Returns instance of implementing class identified by given id.
         
         @param id identifier
@@ -96,7 +67,7 @@ class GAEModel(JSONSerializable, ndb.Model):
         """update this instance to datastore.
         """
         upsert = kwargs.pop('upsert', False)
-        model = cls.get_by_id(id)
+        model = cls.get(id)
         if model is None:
             if upsert is False:
                 return None
@@ -107,24 +78,11 @@ class GAEModel(JSONSerializable, ndb.Model):
         model.put()
         return model
 
-
     @classmethod
     def delete(cls, id):
         """Delete this instance from datastore.
         """
-        cls.get_by_id(id).key.delete()
-
-    def get_key(self):
-        """Returns the key for this instance.
-        https://cloud.google.com/appengine/docs/standard/python/ndb/creating-entity-keys
-        """
-        return self.key.urlsafe()
-
-    def get_parent_key(self):
-        """Returns this instance's parent key.
-        """
-        return self.key.parent().urlsafe()
-
+        cls.get(id).key.delete()
 
     @property
     def id(self):
@@ -133,26 +91,6 @@ class GAEModel(JSONSerializable, ndb.Model):
     def to_json(self):
         rv = self.to_dict()
         rv['id'] = self.id
-        return rv
-
-class DescendentModel(GAEModel):
-    __parent__ = None
-    def __init__(self, **kwargs):
-        super(DescendentModel, self).__init__(**kwargs)
-        if self.__parent__ is None:
-            raise TypeError('Parent is not defined!')
-
-    """For models that require an ancestor.
-    """
-    def _pre_put_hook(self):
-        """Make sure all descendents have an ancestors.
-        """
-        if not isinstance(self.key.parent, self.__parent__):
-            raise ValueError('Descendents require an ancestor (e.g. parent)')
-
-    def to_json(self):
-        rv = super(DescendentModel, self).to_json()
-        rv['id'] = self.key.parent.id()
         return rv
 
 
@@ -164,9 +102,19 @@ class User(GAEModel):
     avatar_url = ndb.StringProperty()
     oauth = ndb.StringProperty(required=True, indexed=True)
 
-class Bit(GAEModel):
-    __parent__ = User
+    def to_json(self):
+        """Returns the serializable JSON version of this model.
+        NOTE: Flask uses this to serialize for jsonify and session.
+        To prevent oauth token from being exposed to client (via jsonify)
+        we remove it before being serialized.
+        """
+        rv = super(User, self).to_json()
+        del rv['oauth'] # hide oauth from client view
+        return rv
 
+
+class Bit(GAEModel):
+    user = ndb.KeyProperty(required=True, kind=User)
     title = ndb.StringProperty(required=True)
     content = ndb.StringProperty(required=True)
     tags = ndb.StringProperty(repeated=True)
@@ -175,8 +123,7 @@ class Bit(GAEModel):
     updated_at = ndb.DateTimeProperty(auto_now=True)
 
     @classmethod
-    def update(cls, user_id, id, **kwargs):
-        user_key = User.id_to_key(user_id)
-        return super(Bit, cls).update(id, parent=user_key, **kwargs)
+    def update(cls, id, user_id, **kwargs):
+        return super(cls, Bit).update(id, user=User.id_to_key(user_id), **kwargs)
 
 
--- server/services.py
+++ server/services.py
@@ -1,231 +1,111 @@
 import logging
 import re
 
+from datetime import datetime
 from abc import abstractmethod, ABCMeta
 from flask import current_app, app
 from flask_github import GitHub
 from pymongo import MongoClient
 from werkzeug.contrib.cache import SimpleCache
+from .models import User, Bit
 
 
 # shared db instance
-db = MongoClient()['bits_db']
+# db = MongoClient()['bits_db']
 # shared github instance
 github = GitHub()
 # shared cache instance
 cache = SimpleCache()
 log = logging.getLogger(__name__)
 
-
-class UserDao(object):
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def get(self, id):
-        """Return user with given id or None if user does not exists."""
-        pass
-
-    @abstractmethod
-    def list(self, filter=None, skip=0, limit=100):
-        """Return list of users."""
-        pass
-    
-    @abstractmethod
-    def save(self, user):
-        """Saves a new user."""
-        pass
-
-    @abstractmethod
-    def update(self, id, **kwargs):
-        """Update user with given id."""
-        pass
-
-    @abstractmethod
-    def delete(self, id):
-        """Delete user with given id."""
-        pass
-
-
-class BitDao(object):
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def get(self, user_id, id):
-        """Return bit with id and owned by or None if bit does not exists."""
-        pass
-
-    @abstractmethod
-    def list(self, user_id, filter=None, skip=0, limit=100):
-        """Return list of bits owned by user."""
-        pass
-    
-    @abstractmethod
-    def save(self, user_id, bit):
-        """Saves a bit for user."""
-        pass
-
-    @abstractmethod
-    def update(self, user_id, id, **kwargs):
-        """Update bit with given id and owned by user."""
-        pass
-
-    @abstractmethod
-    def delete(self, user_id, id):
-        """Delete bit with given id and owned by user."""
-        pass
-
-
-class UserMongoDao(UserDao):
-
-    def __init__(self, db, collection_name='users', **kwargs):
-        self._db = db
-        self._collection_name = collection_name
-        self._collection = db[collection_name]
-
-    def get(self, id):
-        return self._collection.find_one({'_id': id}, projection={'bits': False})
-
-    def list(self, filter=None, skip=0, limit=100):
-        return self._collection.find(filter=filter, projection={'bits': False}, skip=skip, limit=limit)
-    
-    def save(self, user):
-        rv = self._collection.insert_one(user)
-        return user
-
-    def update(self, id, **kwargs):
-        rv = self._collection.update_one({'_id': id}, {'$set': kwargs})
-        return rv.modified_count == 1
-
-    def delete(self, id):
-        rv = self._collection.delete_one({'_id': id})
-        return rv.deleted_count == 1
-
-
-class BitMongoDao(BitDao):
-    _user_bit_projection = {'oauth': False}
-    _user_all_bits_projection = {'oauth': False}
-
-    def __init__(self, db, collection_name='users', **kwargs):
-        self.db = db
-        self._collection_name = collection_name
-        self._collection = db[collection_name]
-
-    def get(self, user_id, id):
-        return self._collection.find_one(filter={'_id': user_id},
-            projection={'name': 1, 'avatar_url': 1, 'bits': {'$elemMatch': {'_id': id}}})
-
-    def list(self, user_id, filter=None, skip=0, limit=100):
-        # combine required user_id and optional filter into 1 filter
-        filter = dict(filter or {})
-        filter.update({'_id': user_id})
-        return self._collection.find_one(
-            filter=filter, projection={'oauth': 0, 'bits.content': 0})
-        # return self._collection.aggregate([
-        #     {'$match': filter},
-        #     {'$project': {
-        #         'name': 1,
-        #         'avatar_url': 1,
-        #         'bits._id': 1,
-        #         'bits.description': 1,
-        #         'bits.created_at': 1,
-        #         'bits.updated_at': 1,
-        #         'bits.published': 1,
-        #         'bits.published_at': 1,
-        #         'bits.tags': 1,
-        #         'authenticated': {'$eq': ['$_id', user_id]}
-        #     }}], batchSize=1)
-            
-    def save(self, user_id, bit):
-        rv = self._collection.update_one(
-                {'_id': user_id, 'bits._id': {'$ne': bit['_id']}}, # ensure not duplicate
-                {'$push': {'bits': bit}})
-        if rv.modified_count != 1 or rv.matched_count != 1:
-            raise ValueError('Unable to add new bit! modified=%d modified=%d, bit=%s' % 
-                (rv.matched_count, rv.modified_count, bit))
-        return bit
-        
-    def update(self, user_id, id, **kwargs):
-        rv = self._collection.update_one(
-            {'_id': user_id, 'bits._id': {'$eq': id}},
-            {'$set': {'bits.$[bit]': kwargs}},
-            array_filters=[{'bit._id': {'$eq': id}}])
-        return rv.modified_count == 1
-
-    def delete(self, user_id, id):
-        rv = self._collection.update_one({'_id': user_id}, 
-            {'$pull': {'bits': {'_id': id}}})
-        return rv.modified_count == 1
+DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+BLANK_BIT = {}
 
 
 class UserService(object):
-    def __init__(self, dao, **kwargs):
-        self._dao = dao
+    __model__ = User
 
-    def get(self, id):
-        return self._dao.get(id)
+    @classmethod
+    def get(cls, id):
+        return cls.__model__.get(id)
 
-    def update(self, id, **kwargs):
-        return self._dao.update(id, **kwargs)
-
-    def save(self, user):
-        return self._dao.save(user)
-
-    def delete(self, id):
-        return self._dao.delete(id)
-
-    def list(self, filter=None, skip=0, limit=100):
-        return self._dao.list(filter=filter, skip=skip, limit=limit)
+    @classmethod
+    def update(cls, id, **kwargs):
+        return cls.__model__.update(id, **kwargs)
 
 
 class BitService(object):
-    def __init__(self, dao, **kwargs):
-        self._dao = dao
+    __model__ = Bit
 
-    def _fetch_all_from_github(self, user_id):
+    @classmethod
+    def get(cls, id):
+        return cls.__model__.get(id)
+
+    @classmethod
+    def _fetch_all_from_github(cls, user_id):
         """TODO: only able to fetch currently authenticated user's bits."""
         gists = github.get('/gists')
         for gist in gists:
-            if self._is_bit(gist):
-                full_gist = self._fetch_one_from_github(gist['id'])
-                bit = self._to_bit_from_gist(full_gist)
-                self._dao.save(user_id, bit)
+            if cls._is_bit(gist):
+                full_gist = cls._fetch_one_from_github(gist['id'])
+                cls._to_bit_from_gist(full_gist, upsert=True)
 
-    def _fetch_one_from_github(self, gist_id):
+    @classmethod
+    def _fetch_one_from_github(cls, gist_id):
         return github.get('/gists/' + gist_id)
 
-    def _is_bit(self, gist):
+    @classmethod
+    def _is_bit(cls, gist):
         return 'bit.md' in gist['files'].keys()
 
-    def _patch_to_github(self, id, bit):
-        url = '/gists/' + id
-        gist_data = self._build_gist_data(bit)
-        gist = github.patch(url, data=gist_data)
-        return self._to_bit_from_gist(gist)
+    @classmethod
+    def _patch_to_github(cls, id, gist_data):
+        return github.patch('/gists/' + id, data=gist_data)
         
-    def _post_to_github(self, bit):
-        gist_data = self._build_gist_data(bit)
-        gist = github.post('/gists', data=gist_data)
-        return self._to_bit_from_gist(gist)
+    @classmethod
+    def _post_to_github(cls, gist_data):
+        return github.post('/gists', data=gist_data)
 
-    def _to_bit_from_gist(self, gist):
-        files = gist['files']
-        meta = self._parse_meta(files['_meta_'])
-        return {
-            '_id': gist['id'],
-            'description': gist['description'],
-            'created_at': gist['created_at'],
-            'updated_at': gist['updated_at'],
-            'published': meta['published'],
-            'published_at': meta['published_at'],
-            'tags': meta['tags'],
-            'content': files['bit.md']['content']
-        }
+    @classmethod
+    def _to_bit_from_gist(cls, gist, upsert=False):
+        # TODO: validate upstream
+        raw_bit_file = gist.get('files', {}).get('bit.md')
+        meta, content = cls._parse_raw_bit_file(raw_bit_file)
+        return cls.__model__.update(
+                id=gist['id'], 
+                user_id=gist['owner']['login'], 
+                title=gist['description'],
+                tags=meta.get('tags', []),
+                updated_at=cls._parse_datetime(gist['updated_at']),
+                created_at=cls._parse_datetime(gist['created_at']),
+                published_at=cls._parse_datetime(meta['published_at']),
+                content=content,
+                upsert=upsert)
 
-    def _parse_meta(self, meta):
-        tokens = re.split('\n', meta['content'])
+    @classmethod
+    def _parse_datetime(cls, s):
+        return datetime.strptime(s, DATETIME_FORMAT) if s else None
+
+    @classmethod
+    def _parse_raw_bit_file(cls, bit_file):
+        """Splits the yaml front matter (meta) and markdown (content).
+
+        @param bit_file raw bit file from gist
+        @returns tuple of (meta, content)
+        """
+        meta = None
+        content = None
+        matches = re.search(r'^---(.*?)---\s*(.*)', bit_file['content'], re.DOTALL)
+        if matches:
+            meta = cls._normalize_meta(matches.groups()[0])
+            content = matches.groups()[1] if len(matches.groups()) >= 2 else None
+        return (meta, content)
+ 
+    @classmethod
+    def _normalize_meta(cls, meta):
+        tokens = re.split('\n', meta)
         tags = None
-        published = False
-        date = None
+        published_at = None
 
         for t in tokens:
             t = t.strip()
@@ -233,79 +113,52 @@ class BitService(object):
                 continue
             if t.startswith('tags:'):
                 tags = filter(None, re.split(',\s+', t[5:].strip()))
-            elif t.startswith('published:'):
-                t = t[10:].strip()
-                published = t or False        
             elif t.startswith('published_at:'):
                 t = t[13:].strip()
-                date = t or None
-        return {'tags': tags, 'published': published, 'published_at': date}
+                published_at = t or None
 
 
-    def _to_gist_from_bit(self, bit):
+        return {'tags': tags, 'published_at': published_at}
+
+    @classmethod
+    def _build_gist_data(cls, bit_data=BLANK_BIT):
         return {
-            'description': bit['description'],
+            'description': bit_data.get('title',''),
             'files': {
-                '_meta_': {'content': self._denormalize_meta(bit)},
-                'bit.md': {'content': bit['content']}
+                'bit.md': {'content': cls._denormalize_meta(bit_data) + bit_data.get('content','')}
             }
         }
 
-    def _build_gist_data(self, bit):
-        return {
-            'description': bit['description'],
-            'files': {
-                '_meta_': {'content': self._denormalize_meta(bit)},
-                'bit.md': {'content': bit['content']}
-            }
-        }
+    @classmethod
+    def _denormalize_meta(cls, bit_data):
+        return  '---\n' \
+                + 'title:' + bit_data.get('title', '') + '\n' \
+                + 'tags:' + ','.join(bit_data.get('tags', '')) + '\n' \
+                + 'published_at:' + bit_data.get('published_at','') + '\n' \
+                + '---\n'
+    
+    @classmethod
+    def update(cls, id, **kwargs):
+        # convert bit -> gist data -> github -> gist -> bit model -> ndb
+        gist_data = cls._build_gist_data(**kwargs)
+        gist = cls._patch_to_github(id, gist_data)
+        return cls._to_bit_from_gist(gist)
 
-    def _denormalize_meta(self, bit):
-        return 'tags:' + (','.join(bit['tags']) if bit['tags'] else '') + '\n' \
-                + 'published:' + str(bit['published'] or '') + '\n' \
-                + 'published_at:' + (bit['published_at'] or '')
+    @classmethod
+    def create(cls, **kwargs):
+        # convert bit -> gist data -> github -> gist -> bit model -> ndb
+        gist_data = cls._build_gist_data(**kwargs)
+        gist = cls._post_to_github(gist_data)
+        return cls._to_bit_from_gist(gist, upsert=True)
 
-    def update(self, user_id, id, **kwargs):
-        """Convert bit -> gist data -> github -> gist -> bit."""
-        bit = self._patch_to_github(id, kwargs)
-        return self._dao.update(user_id, id, **bit)
+    @classmethod
+    def list(cls, user_id):
+        return cls.__model__.list()
 
-    def create(self, user_id, **kwargs):
-        """Creates a brand new bit with defaults."""
-        if kwargs is None:
-            return self.save(user_id, {
-                'description': 'Enter description here',
-                'content': 'Enter markdown here',
-                'tags': [],
-                'published': None,
-                'published_at': None})
-        else:
-            return self.save(user_id, kwargs)
+    @classmethod
+    def delete(cls, id):
+        cls.__model__.delete(id)
 
-    def save(self, user_id, bit):
-        bit = self._post_to_github(bit)
-        return self._dao.save(user_id, bit)
-        
-    def sync(self, user_id):
-        self._fetch_all_from_github(user_id)
-
-    def delete(self, user_id, id):
-        resp = github.delete('/gists/' + id)
-        if resp.status_code is 204:
-            self._dao.delete(user_id, id)
-            return id
-        else:
-            log.warn('Nothing to delete for %s, reason=%s' % (id, resp.status_code))
-            raise RuntimeError('Unable to delete %s, reason=%s', (resp.status_code))
-        
-    def get(self, user_id, id):
-        return self._dao.get(user_id, id)
-
-    def list(self, user_id, filter=None, skip=0, limit=100):
-        return self._dao.list(user_id, filter, skip, limit)
-
-
-# shared user service impl
-user_service = UserService(UserMongoDao(db))
-# shared bit service impl
-bit_service = BitService(BitMongoDao(db))
+    @classmethod
+    def sync(cls, user_id):
+        cls._fetch_all_from_github(user_id)
