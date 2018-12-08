03ff4c9b88b8fbaac503941a66cf193ccf86f889
ikumen <ikumen@gnoht.com>
2018-12-07 23:42:21 -0800

    Isolating webpack css loading issue.

--- webpack.config.js
+++ webpack.config.js
@@ -24,21 +24,21 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: "[name]_[local]_[hash:base64]",
-              sourceMap: true,
-              minimize: true
-            }
-          }          
-        ]
+        use: ["style-loader", "css-loader"
+          // {
+          //   loader: "style-loader"
+          // },
+          // {
+          //   loader: "css-loader",
+          //   options: {
+          //     modules: true,
+          //     importLoaders: 1,
+          //     localIdentName: "[name]_[local]_[hash:base64]",
+          //     sourceMap: true,
+          //     // minimize: true
+          //   }
+          // }          
+        ],
       }
     ]
   },
